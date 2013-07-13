880ffb0711fd7246f270ebbb6b30729910f83045
Chang W. Doh <changwook.doh@gmail.com>
2013-07-14 05:00:37 +0900

    deploy site updates

--- demo/cssparser.js
+++ demo/cssparser.js
@@ -1,4 +1,4 @@
-/** cssparser - v0.2.1 - 2013-06-06
+/** cssparser - v0.2.1 - 2013-07-14
  * http://cwdoh.github.io/cssparser.js/
  * Git Build: SHA1 : 47eb8060bc097b9b0a17cfe38dff3eafce286b9b
  * Copyright (c) 2013 Chang W. Doh; Licensed MIT, http://www.opensource.org/licenses/mit-license.php
